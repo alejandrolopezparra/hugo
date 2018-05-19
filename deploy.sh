d759a8ea795ba93c500811958c0688565b4e6244
Alejandro Lopez Parra <alejandrolopezparra@users.noreply.github.com>
2018-05-19 22:19:33 +0200

    Improving deployment script

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,6 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 
-##### HUGO
-
-# Add changes to git.
-git add --all
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-
 ##### WEB
 
 # Go To Public folder
@@ -33,10 +20,25 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
+# Push source and build repos.
+git push origin master
 
 
 # Come Back up to the Project Root
 cd ..
+
+
+##### HUGO
+
+# Add changes to git.
+git add --all
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
